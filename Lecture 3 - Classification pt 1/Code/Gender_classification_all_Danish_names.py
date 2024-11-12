c281490b6844a6e3d48050b2d621a3761c7c17e0
christianvedels <christian-vs@sam.sdu.dk>
2024-11-12 16:19:40 +0100

    Update lecture

--- Lecture 3 - Classification pt 1/Code/Gender_classification_all_Danish_names.py
+++ Lecture 3 - Classification pt 1/Code/Gender_classification_all_Danish_names.py
@@ -4,10 +4,6 @@ Created on Mon Nov 13 10:56:51 2023
 
 @author: chris
 """
-# %% Directory
-import os
-script_directory = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_directory)
 
 # %% Libraries
 import pandas as pd
@@ -20,66 +16,66 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.feature_extraction import DictVectorizer
 import matplotlib.pyplot as plt
 import seaborn as sns
+import unidecode
 
-# %% Load data
-male   = pd.read_table("../Data/names_males.txt", sep = "\t", encoding='ISO-8859-1')
-female = pd.read_table("../Data/names_females.txt", sep = "\t", encoding='ISO-8859-1') 
+# %% Functions
+def clean_strings(x):
+    # Cast to string
+    x = [str(y) for y in x] # Cast to string
+    x = [y.lower() for y in x] # Lower case
+    x = [unidecode.unidecode(y) for y in x] # Remove weird letters
+    
+    return(x)
 
-# %% Data wrangling
-# Drop NA 
-male = male.dropna()
-female = female.dropna()
+def clean_data(male, female):
+    # Drop NA 
+    male = male.dropna()
+    female = female.dropna()
+    
+    # Upsample
+    male = pd.DataFrame({
+        'clean_name': male['clean_name'].repeat(male['ANTAL'])
+    })
+    
+    female = pd.DataFrame({
+        'clean_name': female['clean_name'].repeat(female['ANTAL'])
+    })
+        
+    # Add gender labels to the data
+    male['gender'] = 'male'
+    female['gender'] = 'female'
+    
+    # Combine male and female datasets
+    list_males   = list(zip(male['clean_name'], male['gender']))
+    list_females = list(zip(female['clean_name'], female['gender']))
+    labeled_names = list_males + list_females
+    
+    # Shuffle
+    r.seed(20)
+    r.shuffle(labeled_names)
+    
+    # Features
+    X = [name for (name, gender) in labeled_names]
+    y = [gender for (name, gender) in labeled_names]
+    
+    # # Convert labels
+    label_encoder_y = LabelEncoder()
+    y = label_encoder_y.fit_transform(y)
+    label_encoder_X = LabelEncoder()
+    X = label_encoder_X.fit_transform(X)
+    
+    # # Reshape X
+    X = np.array(X).reshape(-1, 1)
+    
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
+    
+    # Return this and the encoders
+    return X_train, X_test, y_train, y_test, label_encoder_y, label_encoder_X
 
-# Upsample
-male = pd.DataFrame({
-    'Navn': male['Navn'].repeat(male['ANTAL'])
-})
-
-female = pd.DataFrame({
-    'Navn': female['Navn'].repeat(female['ANTAL'])
-})
-
-# Names to lower
-male['Navn'] = [i.lower() for i in male['Navn'].tolist()]
-female['Navn'] = [i.lower() for i in female['Navn'].tolist()]
-
-# Add gender labels to the data
-male['gender'] = 'male'
-female['gender'] = 'female'
-
-# Combine male and female datasets
-list_males   = list(zip(male['Navn'], male['gender']))
-list_females = list(zip(female['Navn'], female['gender']))
-labeled_names = list_males + list_females
-
-# Shuffle
-r.seed(20)
-r.shuffle(labeled_names)
-
-# Features
-X = [name for (name, gender) in labeled_names]
-y = [gender for (name, gender) in labeled_names]
-
-# # Convert labels
-label_encoder_y = LabelEncoder()
-y = label_encoder_y.fit_transform(y)
-label_encoder_X = LabelEncoder()
-X = label_encoder_X.fit_transform(X)
-
-# # Reshape X
-X = np.array(X).reshape(-1, 1)
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
-
-# %% Classifier
-# Initialize and train the scikit-learn Naive Bayes classifier
-classifier = CategoricalNB()
-classifier.fit(X_train, y_train)
-
-# %%
-# Making classifier wrapper function
+# Making classifier wrapper function (will be used to apply the classificer easily)
 def classifier_wrapper(x):
+    # NOTE: label_encoder_y and label_encoder_X must be available in environment
     # Contain all pre/post processing steps*
     x = [i.lower() for i in x]
     x = label_encoder_X.transform(x)
@@ -88,117 +84,79 @@ def classifier_wrapper(x):
     res = label_encoder_y.inverse_transform(res)
     return res
 
-# %%
-# Classify some examples
+# %% Main
+if __name__ == "__main__":
+    
+    # Load modern names data
+    male   = pd.read_table("../Data/names_males.txt", sep = "\t", encoding='ISO-8859-1')
+    female = pd.read_table("../Data/names_females.txt", sep = "\t", encoding='ISO-8859-1') 
+    
+    male['clean_name'] = clean_strings(male.Navn.tolist())
+    female['clean_name'] = clean_strings(female.Navn.tolist())
+    
+    # Get train / test data and encoders
+    X_train, X_test, y_train, y_test, label_encoder_y, label_encoder_X = clean_data(male, female)
+    
+    # Initialize and 'train' the scikit-learn Naive Bayes classifier
+    classifier = CategoricalNB()
+    classifier.fit(X_train, y_train) # Calculates probabilities
+    
+    # Classify some examples
+    print(classifier_wrapper(['Neo']))
+    print(classifier_wrapper(['Trinity']))
+    print(classifier_wrapper(['Casper']))
+    print(classifier_wrapper(['Marie']))
+    
+    # Name of Elon Musk' child
+    try: 
+        print(classifier_wrapper(["XÆA-12"]))
+    except:
+        print("Name not in training data")
+        
+    # Evaluate the accuracy on the test set
+    y_pred = classifier.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
+        
+    # Apply to Danish census data from the year 1787 from Link Lives
+    # https://link-lives.dk/en/about-link-lives/
+    # This used to not contain gender. But we will use it as a test case anyway
+    df1787 = pd.read_csv("../Data/Names_gender1787.csv")
+    print(df1787)
+    
+    # Take only first of first names
+    df1787['clean_name'] = df1787['first_names'].str.split().str[0]
+    df1787['clean_name'] = clean_strings(df1787.clean_name)
+    
+    # Remove unknown names
+    df1787_clean = df1787[df1787['clean_name'].isin(male['clean_name']) | df1787['clean_name'].isin(female['clean_name'])]
+    df1787_removed = df1787[~df1787['clean_name'].isin(male['clean_name']) & ~df1787['clean_name'].isin(female['clean_name'])]
 
-# Errors because of unseen:
-print(classifier_wrapper(['Neo']))
-print(classifier_wrapper(['Trinity']))
-print(classifier_wrapper(['Casper']))
-print(classifier_wrapper(['Marie']))
+    # Print removed/unremoved
+    def N_of(x): 
+        return np.sum(x['n']) 
+    N_all = N_of(df1787)
+    N_kept = N_of(df1787_clean)
 
-# Name of Elon Musk' child
-# print(classifier_wrapper(["XÆA-12"]))
-# Error because it is unseen in traning
+    # Print descriptive
+    print(f"All obs: {N_all}")
+    print(f"Obs. kept: {N_kept}; Pct: {100*round(N_kept/N_all, 4)}%")
+    print(f"Obs. removed: {N_all - N_kept}; Pct: {100*round((N_all - N_kept)/N_all, 4)}%")
 
-# %% Evaluate
-# Evaluate the accuracy on the test set
-y_pred = classifier.predict(X_test)
-print(accuracy_score(y_test, y_pred))
+    # Transform to features
+    X = label_encoder_X.transform(df1787_clean['clean_name'])
+    # Reshape X
+    X = np.array(X).reshape(-1, 1)
 
-# Confusion Matrix
-conf_matrix = confusion_matrix(y_test, y_pred)
-print('Confusion Matrix:')
-print(conf_matrix)
+    # Predict 
+    y_pred = classifier.predict(X)
 
-# Plot Confusion Matrix
-labels = ['female', 'male']
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Confusion Matrix')
-plt.show()
+    # True labels 1787
+    y = label_encoder_y.transform(df1787_clean['gender'])
 
-# Normalize the confusion matrix to percentages
-conf_matrix_pct = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
-print('Normalized Confusion Matrix:')
-print(conf_matrix_pct)
-
-# Plot Normalized Confusion Matrix
-labels = ['female', 'male']
-sns.heatmap(conf_matrix_pct, annot=True, fmt='.2f', cmap='Blues', xticklabels=labels, yticklabels=labels)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Normalized Confusion Matrix (%)')
-plt.show()
-
-# %% Test on Danish census data from the year 1787 from Link Lives
-# https://link-lives.dk/en/about-link-lives/
-# This used to not contain gender. Now it does.
-df1787 = pd.read_csv("../Data/Names_gender1787.csv")
-print(df1787)
-
-# Take only first of first names
-df1787['clean_names'] = df1787['first_names'].str.split().str[0]
-
-# Remove unknown names
-df1787_clean = df1787[df1787['clean_names'].isin(male['Navn']) | df1787['clean_names'].isin(female['Navn'])]
-df1787_removed = df1787[~(df1787['clean_names'].isin(male['Navn']) | df1787['clean_names'].isin(female['Navn']))]
-
-# Print removed/unremoved
-def N_of(x): 
-    return np.sum(x['n']) 
-N_all = N_of(df1787)
-N_removed = N_of(df1787_removed) 
-N_kept = N_of(df1787_clean)
-
-# Print descriptive
-print(f"All obs: {N_all}")
-print(f"Obs. kept: {N_kept}; Pct: {100*round(N_kept/N_all, 4)}%")
-print(f"Obs. removed: {N_removed}; Pct: {100*round(N_removed/N_all, 4)}%")
-
-# Transform to features
-X = label_encoder_X.transform(df1787_clean['clean_names'])
-# Reshape X
-X = np.array(X).reshape(-1, 1)
-
-# Predict 
-y_pred = classifier.predict(X)
-
-# ==== Test performance ====
-# True labels 1787
-y = label_encoder_y.transform(df1787_clean['gender'])
-
-# Accuracy estimated with weights from population counts in 1787
-weights = df1787_clean['n'].tolist()
-accuracy = accuracy_score(y, y_pred, sample_weight = weights)
-print(f"\nAccuracy of Naïve Bayes model on 1787 data: {100*round(accuracy, 4)}%")
-print(f"Corrected for unfound names: {100*round(accuracy*N_kept/N_all, 4)}%")
-
-# Confusion Matrix
-conf_matrix = confusion_matrix(y, y_pred, sample_weight = weights)
-print('Confusion Matrix:')
-print(conf_matrix)
-
-# Plot Confusion Matrix
-labels = ['female', 'male']
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Confusion Matrix')
-plt.show()
-
-# Normalize the confusion matrix to percentages
-row_sums = conf_matrix.sum(axis=1)[:, np.newaxis]
-conf_matrix_pct = conf_matrix.astype('float') / row_sums
-print('Normalized Confusion Matrix:')
-print(conf_matrix_pct)
-
-# Plot Normalized Confusion Matrix
-labels = ['female', 'male']
-sns.heatmap(conf_matrix_pct, annot=True, fmt='.4f', cmap='Blues', xticklabels=labels, yticklabels=labels)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Normalized Confusion Matrix (%)')
-plt.show()
+    # Accuracy estimated with weights from population counts in 1787
+    weights = df1787_clean['n'].tolist()
+    accuracy = accuracy_score(y, y_pred, sample_weight = weights)
+    
+    print(f"\nAccuracy of Naïve Bayes model on 1787 data: {100*round(accuracy, 4)}%")
+    print(f"Corrected for unfound names: {100*round(accuracy*N_kept/N_all, 4)}%")
 
